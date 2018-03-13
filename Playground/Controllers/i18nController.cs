ea46d8c05aa46cac3e8a2931f013cab87231cb08
mRRa <spiriturl@gmail.com>
2018-03-13 22:26:11 +0800

    i18n & dropdownlist

---
+++ Playground/Controllers/i18nController.cs
@@ -0,0 +1,40 @@
+﻿using Playground.ViewModels;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using System.Web.Mvc;
+
+namespace Playground.Controllers
+{
+    public class i18nController : Controller
+    {
+        //all http method, i18n
+        public ActionResult Index(string selectedLang = "zh-TW")
+        {
+            i18nViewModel model = new i18nViewModel();
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(selectedLang);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(selectedLang);
+
+            //Language options
+            SelectList items = new SelectList(
+                items: new List<SelectListItem>{
+                    new SelectListItem{ Text = "繁體中文", Value = "zh-TW" },
+                    new SelectListItem{ Text = "English", Value = "en-US" },
+                    new SelectListItem{ Text = "日本語", Value = "ja-JP"}
+                },
+                dataTextField: "Text",
+                dataValueField: "Value"
+            );
+            model.Languages = items;
+
+            //Selected option
+            model.Selected = selectedLang;
+
+            //Please Select at first option
+            model.Display = Resources.ChooseLang;
+
+            return View(model);
+        }
+    }
+}
---
+++ Playground/ViewModels/i18nViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Playground.ViewModels
+{
+    public class i18nViewModel
+    {
+        public SelectList Languages { get; set; }
+
+        public string Display { get; set; }
+
+        public string Selected { get; set; }
+    }
+}
