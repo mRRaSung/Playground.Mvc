8d28669d8c555e287bc73f0011fc94b9f4f4f7f1
mRRa <spiriturl@gmail.com>
2018-03-13 23:30:19 +0800

    i18n viewmodel summary

--- Playground/ViewModels/i18nViewModel.cs
+++ Playground/ViewModels/i18nViewModel.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Playground.ViewModels
 {
     public class i18nViewModel
     {
+        /// <summary>
+        /// Language options
+        /// </summary>
         public SelectList Languages { get; set; }
-
+        /// <summary>
+        /// Language dislpay text when unselect
+        /// </summary>
         public string Display { get; set; }
-
+        /// <summary>
+        /// Selected option
+        /// </summary>
         public string Selected { get; set; }
     }
 }
