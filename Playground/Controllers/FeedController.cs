270b36d46c369fc5862f1ee58143d62835412feb
mRRa <spiriturl@gmail.com>
2018-03-15 22:16:28 +0800

    Add rss action result (dislpay with ie, firefox)

---
+++ Playground/Controllers/FeedController.cs
@@ -0,0 +1,52 @@
+﻿using Playground.Infrastructure.ActionResults;
+using Playground.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+
+namespace Playground.Controllers
+{
+    public class FeedController : Controller
+    {
+        private readonly NorthwindEntities db = new NorthwindEntities();
+
+        public ActionResult Index()
+        {
+            var feed = GetFeedData();
+
+            return new RssActionResult(feed);
+        }
+
+        private SyndicationFeed GetFeedData()
+        {
+            string hostUrl = string.Format($"{Request.Url.Scheme}://{Request.Url.Authority}");
+
+            //Rss title and link
+            var feedResult = new SyndicationFeed("RSS", "Example Description", 
+                                           new Uri(string.Concat(hostUrl, "/feed/")));
+
+            List<SyndicationItem> listSynd = new List<SyndicationItem>();
+            SyndicationItem syndication;
+
+            //Collect infos
+            db.Products.ToList().ForEach(product =>
+            {
+                syndication = new SyndicationItem(
+                    string.Concat(product.Categories.CategoryName, " - ", product.ProductName),
+                    string.Format("Quantity per Unit: {0}, Unit Price: {1}", product.QuantityPerUnit, product.UnitPrice),
+                    new Uri(string.Concat(hostUrl, "/Products/Details?id=", product.ProductID)),
+                    "ID",
+                    DateTime.Now);
+
+                listSynd.Add(syndication);
+            });
+
+            //Put infos to syndication
+            feedResult.Items = listSynd;
+
+            return feedResult;
+        }
+    }
+}
---
+++ Playground/Infrastructure/ActionResults/RssActionResult.cs
@@ -0,0 +1,35 @@
+﻿using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace Playground.Infrastructure.ActionResults
+{
+    public class RssActionResult : ActionResult
+    {
+        /// <summary>
+        /// Rss 資訊聚合
+        /// </summary>
+        private SyndicationFeed Feed;
+
+        public RssActionResult(SyndicationFeed feed)
+        {
+            this.Feed = feed;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            //Set content type
+            context.HttpContext.Response.ContentType = "application/rss+xml";
+
+            //Rss Formatter
+            var formatter = new Rss20FeedFormatter(Feed);
+
+            //Output by Rss formatter
+            using (var writer = XmlWriter.Create(context.HttpContext.Response.Output))
+            {
+                formatter.WriteTo(writer);
+            }
+
+        }
+    }
+}
