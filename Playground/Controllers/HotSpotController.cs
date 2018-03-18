8e587094c0dc780307acef7d55fb63f52a00f292
mRRa <spiriturl@gmail.com>
2018-03-18 21:50:17 +0800

    task<actinoresult>

---
+++ Playground/Controllers/HotSpotController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Playground.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Playground.Controllers
+{
+    public class HotSpotController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            string targetUrl = "http://data.ntpc.gov.tw/od/data/api/04958686-1B92-4B74-889D-9F34409B272B?$format=json";
+
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = Int32.MaxValue;
+
+            var response = await client.GetStringAsync(targetUrl);
+
+            //ViewBag.Result = response;
+            var collection = JsonConvert.DeserializeObject<IEnumerable<HotSpotViewModel>>(response);
+
+            return View(collection);
+        }
+    }
+}
---
+++ Playground/ViewModels/HotSpotViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Playground.ViewModels
+{
+    public class HotSpotViewModel
+    {
+        [Display(Name = "編號")]
+        public string id { get; set; }
+        [Display(Name = "名稱")]
+        public string spot_name { get; set; }
+        public string type { get; set; }
+        public string company { get; set; }
+        public string district { get; set; }
+        public string address { get; set; }
+        public string apparatus_name { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string twd97X { get; set; }
+        public string twd97Y { get; set; }
+        public string wgs84aX { get; set; }
+        public string wgs84aY { get; set; }
+    }
+}
